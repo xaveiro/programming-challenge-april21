6d682477c7dd6c021a2e3e872b210db57d2ac0eb
xaveiro <vhpx@icomp.ufam.edu.br>
2021-05-03 08:15:17 -0400

    fix router for get movies by year and genre

--- api/routes/router.js
+++ api/routes/router.js
@@ -3,7 +3,7 @@ const movieController = require("../controllers/movies");
 const ratingController = require("../controllers/ratings");
 const router = express.Router();
 
-router.get("/movies/year=year&genre=genre", movieController.getMoviesByYearAndGenre);
+router.get("/movies/year:year&genre:genre", movieController.getMoviesByYearAndGenre);
 router.get("/avgRating=ratings", ratingController.getTopKRatedMovies);
 
 
